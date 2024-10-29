8164f69e7d2a5083358a4af271292b841dff34c4
javid235 <mohamedjavid235@gmail.com>
2024-10-29 16:55:06 +0530

    #

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Account.css'
 
 const Account = () => {
+
+  const [account, setAccount]= useState("Login")
+
+  const changeHandler = ()=>{
+    if (account === 'Sign Up') {
+      setAccount('Login')
+    } else {
+      setAccount('Sign Up')
+    }
+  }
   return (
     <div className='acc-logsign-main'>
       <div className='acc-logsign-cont'>
-        <h1>CREATE ACCOUNT</h1>
-        <input type='text' placeholder='Name' />
+        {account === 'Sign Up'?<h1>CREATE ACCOUNT</h1>:<h1>LOGIN</h1>}
+        {account === 'Sign Up'?<input type='text' placeholder='Full Name' />:<></>}
+        {account === 'Sign Up'?<input type='text' placeholder='User Name' />:<></>}
         <input type='email' placeholder='Email' />
         <input type='password' placeholder='Password' />
-        {/* <input type='password' placeholder='Confirm Password' /> */}
+        {account === 'Sign Up'?<input type='password' placeholder='Confirm Password' /> :<></>}
+        
         <div className='acc-btn-cont'>
-          <button className='acc-signup'>Sign Up</button>
+          {account === 'Sign Up'?<button className='acc-signup'>Sign Up</button>:<button className='acc-signup'>Login</button>}
         </div>
         <div className='acc-alr-login'>
-          Already have an account? <span className='alr-login-btn'>Log in</span>
+          {account === 'Sign Up'?<p>Already have an account? <span className='alr-login-btn' onClick={()=>{changeHandler()}}>Log in</span></p>:<p>Create a Account? <span className='alr-login-btn' onClick={()=>{changeHandler()}}>click Here</span></p>}
         </div>
       </div>
     </div>
